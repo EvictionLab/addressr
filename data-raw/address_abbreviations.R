d93210e67744ab45163c1ae5b6bea917b5583c36
Sarah Johnson <sarahviamemphis@gmail.com>
2024-09-05 20:37:35 -0400

    feat: table of common address abbreviations - data-raw/ has the code to create the dataset - data/ has the output rda data included in pkg - update description to include lazydata - documentation in R/data.R

---
+++ data-raw/address_abbreviations.R
@@ -0,0 +1,39 @@
+## code to prepare `address_abbreviations` dataset goes here
+
+# Directions
+directions <- tribble(
+  ~short, ~long,
+  "N", "NORTH",
+  "S", "SOUTH",
+  "E", "EAST",
+  "W", "WEST",
+  "NE", "NORTHEAST",
+  "NW", "NORTHWEST",
+  "SE", "SOUTHEAST",
+  "SW", "SOUTHWEST"
+) |> mutate(type = "directions")
+
+# USPS Street Endings
+
+library(rvest)
+library(tidyr)
+
+html <- read_html("https://pe.usps.com/text/pub28/28apc_002.htm")
+street_endings_raw <- html |> html_elements(".Basic_no_title") |> html_table(header = TRUE)
+street_endings_raw <- street_endings_raw[[1]] |> janitor::clean_names()
+
+all_street_endings <- street_endings_raw |>
+  pivot_longer(1:2, names_to = NULL, values_to = "value") |>
+  distinct() |>
+  rename(short = 1, long = 2) |>
+  filter(short != long) |>
+  mutate(type = "all_street_ends")
+
+official_endings <- street_endings_raw |>
+  select(short = 3, long = 1) |> distinct() |>
+  filter(short != long) |>
+  mutate(type = "official_street_ends")
+
+address_abbreviations <- bind_rows(directions, all_street_endings, official_endings)
+
+usethis::use_data(address_abbreviations, overwrite = TRUE)
