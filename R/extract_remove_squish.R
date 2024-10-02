781cc65d502f631c069c1719d779f0e4581d26fe
Sarah Johnson <sarah.johnson@princeton.edu>
2024-10-02 19:44:47 -0400

    feat: update extract_remove_squish to remove non-word characters from the start and end of a string

--- R/extract_remove_squish.R
+++ R/extract_remove_squish.R
@@ -20,7 +20,7 @@ extract_remove_squish <- function(.data, original_column, new_column, pattern) {
   .data |>
     mutate(
       {{ new_column }} := str_extract({{ original_column }}, pattern),
-      {{ original_column }} := str_remove({{ original_column }}, pattern) |> str_squish()
+      {{ original_column }} := str_remove({{ original_column }}, pattern) |> str_remove_all("^\\W+|\\W$") |> str_squish()
     )
 }
 
@@ -38,10 +38,14 @@ extract_remove_squish_db <- function(.data, original_column, new_column, pattern
 
   extract_sql <- paste0("regexp_extract(", original_column, ", '", pattern, "')")
   remove_squish_sql <- paste0("trim(regexp_replace(", original_column, ", '", pattern, "', ''))")
+  remove_messy_sql <- paste0("regexp_replace(", original_column, ", '(^\\W+|\\W+$)', '', 'g'")
 
   .data |>
     mutate(
       {{ new_column }} := sql(extract_sql),
       {{ original_column }} := sql(remove_squish_sql)
+    ) |>
+    mutate(
+      {{ original_column }} := sql(remove_messy_sql)
     )
 }
