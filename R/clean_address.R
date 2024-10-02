74aef92839a0ef51ca0395a18dfca38e54c695b1
Sarah Johnson <sarah.johnson@princeton.edu>
2024-10-02 19:48:43 -0400

    feat!: update clean_address to include default method of cleaning addresses - breaks the order of inputs; switch dataset and input_column - feat: spell out numerical street names

--- R/clean_address.R
+++ R/clean_address.R
@@ -8,7 +8,7 @@
 #'    * A modified original column, from which the pattern was removed and whitespace was trimmed.
 #'    * New column containing the extracted strings.
 #' @export
-clean_address <- function(.data, dataset, input_column) {
+clean_address <- function(.data, input_column, dataset = "default") {
 
   if (dataset == "lexis") {
 
@@ -20,7 +20,8 @@ clean_address <- function(.data, dataset, input_column) {
       extract_remove_squish({{ input_column }}, "lexis_building", "building") |>
       extract_remove_squish({{ input_column }}, "lexis_pre_direction", "pre_direction") |>
       extract_remove_squish({{ input_column }}, "lexis_post_direction", "post_direction") |>
-      extract_remove_squish({{ input_column }}, "lexis_all_street_suffix", "all_street_suffix")
+      extract_remove_squish({{ input_column }}, "lexis_all_street_suffix", "all_street_suffix") |>
+      mutate({{ input_column }} := switch_abbreviation({{ input_column }}, "numbered_street_name", "short-to-long"))
 
   } else if (dataset == "lexis_db") {
 
@@ -33,6 +34,38 @@ clean_address <- function(.data, dataset, input_column) {
       extract_remove_squish_db({{ input_column }}, "lexis_pre_direction", "pre_direction") |>
       extract_remove_squish_db({{ input_column }}, "lexis_post_direction", "post_direction") |>
       extract_remove_squish_db({{ input_column }}, "lexis_all_street_suffix", "all_street_suffix")
+
+  } else if (dataset == "default") {
+
+    .data |>
+      mutate({{ input_column }} := str_to_upper({{ input_column }})) |>
+      extract_remove_squish({{ input_column }}, "street_number_fraction", "street_number_fraction") |>
+      extract_remove_squish({{ input_column }}, "street_number_range", "street_number_range") |>
+      extract_remove_squish({{ input_column }}, "street_number", "street_number") |>
+      extract_remove_squish({{ input_column }}, "unit", "unit") |>
+      extract_remove_squish({{ input_column }}, "special_unit", "special_units") |>
+      extract_remove_squish({{ input_column }}, "building", "building") |>
+      extract_remove_squish({{ input_column }}, "pre_direction", "pre_direction") |>
+      extract_remove_squish({{ input_column }}, "post_direction", "post_direction") |>
+      extract_remove_squish({{ input_column }}, "all_street_suffix", "all_street_suffix") |>
+      mutate({{ input_column }} := switch_abbreviation({{ input_column }}, "numbered_street_name", "short-to-long"))
+
+  } else if (dataset == "default_db") {
+
+    df <- .data |>
+      extract_remove_squish_db({{ input_column }}, "street_number_fraction", "street_number_fraction") |>
+      extract_remove_squish_db({{ input_column }}, "street_number_range", "street_number_range") |>
+      extract_remove_squish_db({{ input_column }}, "street_number", "street_number") |>
+      extract_remove_squish_db({{ input_column }}, "unit", "unit") |>
+      extract_remove_squish_db({{ input_column }}, "special_unit", "special_units") |>
+      extract_remove_squish_db({{ input_column }}, "building", "building") |>
+      extract_remove_squish_db({{ input_column }}, "pre_direction", "pre_direction") |>
+      extract_remove_squish_db({{ input_column }}, "post_direction", "post_direction") |>
+      extract_remove_squish_db({{ input_column }}, "all_street_suffix", "all_street_suffix") |>
+      compute()
+
+    df <- df |>
+      switch_abbreviation_db({{ input_column }}, "numbered_street_name", "short-to-long")
 
   }
 
