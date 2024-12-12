031fecf47f73750711353041e1c385f38b68609a
Sarah Johnson <sarah.johnson@princeton.edu>
2024-12-12 16:49:05 -0500

    feat: add tictoc to log times

--- R/addressr-package.R
+++ R/addressr-package.R
@@ -5,5 +5,6 @@
 #' @import rlang dplyr tidyr stringr
 #' @importFrom glue glue
 #' @importFrom purrr map2
+#' @importFrom tictoc tic toc
 ## usethis namespace: end
 NULL
--- R/clean_address.R
+++ R/clean_address.R
@@ -9,6 +9,7 @@
 #'    * New column containing the extracted strings.
 #' @export
 clean_address <- function(.data, input_column, dataset = "default") {
+
   # column names. these prevent global variable warnings
   addressr_id <- sym("addressr_id")
   clean_address <- sym("clean_address")
@@ -24,6 +25,9 @@ clean_address <- function(.data, input_column, dataset = "default") {
   building <- sym("building")
 
   if (dataset == "quick") {
+
+    tic("quick clean address.")
+
     df <- .data |>
       mutate({{ raw_address }} := {{ input_column }}, {{ addressr_id }} := row_number(), .before = {{ input_column }}) |>
       mutate({{ input_column }} := str_to_upper({{ input_column }})) |>
@@ -44,43 +48,50 @@ clean_address <- function(.data, input_column, dataset = "default") {
       mutate({{ input_column }} := na_if({{ input_column }}, "")) |>
       rename("street_name" = {{ input_column }}) |>
       unite("clean_address", c("street_number", "street_number_range", "street_number_fraction", "pre_direction", "street_name", "street_suffix", "post_direction"), sep = " ", remove = FALSE, na.rm = TRUE)
+
+    toc()
+
   }
 
   if (dataset == "default") {
-    original_row_id <- sym("original_row_id")
 
     # step 1: preserve original input
+    tic("preserve original data")
+    original_row_id <- sym("original_row_id")
     df <- .data |>
       mutate({{ raw_address }} := {{ input_column }},
              {{ original_row_id }} := row_number(),
              {{ addressr_id }} := as.character({{ original_row_id }}),
              .before = {{ input_column }}) |>
-      mutate({{ input_column }} := str_to_upper({{ input_column }}))
+      mutate({{ input_column }} := str_to_upper({{ input_column }}) |> str_remove_all("\\.") |> str_replace_all(",", " ") |> str_squish())
+    toc()
 
     # step 2: separate out multiple addresses
-    # current logic: (etc + street suffix) + [punctuation, and, or space] + (numbers + etc + street suffix)
+    tic("separate multiple addresses")
+    # current logic to delim: (etc + street suffix) + [punctuation, and, or space] + (numbers + etc + street suffix)
     all_suffix_regex <- str_collapse_bound(unique(c(all_street_suffixes$long, all_street_suffixes$short)))
-    longer_regex <- paste0("(?<=", all_suffix_regex, ")(\\s)?([:punct:]| AND |\\s)(\\s)?(?=\\d+\\b[\\W\\w\\s]+", all_suffix_regex, ")")
+    longer_regex <- paste0("(?<=", all_suffix_regex, ")\\s*([:punct:]| AND |\\s)\\s*(?=\\d+\\b[\\W\\w\\s]+", all_suffix_regex, ")")
 
     df_multi <- df |> filter(str_detect({{ input_column }}, longer_regex))
-
     df <- df |> anti_join(df_multi, by = "addressr_id")
 
     if (nrow(df_multi) != 0) {
-      addressr_address_id <- sym("addressr_address_id")
+      addressr_addr_id <- sym("addressr_addr_id")
 
       df_multi <- df_multi |>
         separate_longer_delim({{ input_column }}, delim = stringr::regex(longer_regex)) |>
         distinct() |>
-        mutate({{ addressr_address_id }} := row_number(), .by = "addressr_id") |>
-        unite({{ addressr_id }}, c("addressr_id", "addressr_address_id"), sep = "-A", remove = FALSE) |>
-        select(-addressr_address_id)
+        mutate({{ addressr_addr_id }} := row_number(), .by = "addressr_id") |>
+        unite({{ addressr_id }}, c("addressr_id", "addressr_addr_id"), sep = "-A", remove = FALSE) |>
+        select(-addressr_addr_id)
 
       df <- bind_rows(df, df_multi)
 
     }
+    toc()
 
     # step 3: separate out address components from each address & standardize spellings
+    tic("extract address parts")
     # idea: rework the first part & improve street numbers, units, and buildings together
     df <- df |>
       extract_remove_squish({{ input_column }}, "street_number_fraction", "street_number_fraction") |>
@@ -92,30 +103,45 @@ clean_address <- function(.data, input_column, dataset = "default") {
       extract_remove_squish({{ input_column }}, "building", "building") |>
       extract_remove_squish({{ input_column }}, "post_direction", "post_direction") |>
       extract_remove_squish({{ input_column }}, "street_suffix", "all_street_suffix") |>
-      extract_remove_squish({{ input_column }}, "pre_direction", "pre_direction") |>
+      extract_remove_squish({{ input_column }}, "pre_direction", "pre_direction")
+    toc()
+
+    tic("standardize street suffix, directions & ordinals")
+    df <- df |>
       mutate({{ input_column }} := switch_abbreviation({{ input_column }}, "ordinals", "short-to-long")) |>
       mutate({{ street_suffix }} := switch_abbreviation({{ street_suffix }}, "official_street_suffixes", "long-to-short")) |>
       mutate(across(c({{ pre_direction }}, {{ post_direction }}), ~ switch_abbreviation(., "directions", "long-to-short")))
+    toc()
 
-    # see checkers.r for these functions
     # check street number ranges
+    # see checkers.r for these functions
+    tic("check street ranges")
     df <- df |> check_street_range(street_number_multi, street_number, addressr_id)
+    toc()
 
     # check units
+    tic("check units")
     df <- df |>
       check_unit(unit, street_number, street_suffix, addressr_id) |>
       unite({{ unit }}, c("unit", "special_unit"), sep = " ", na.rm = TRUE) |>
       mutate({{ unit }} := str_squish({{ unit }}) |> na_if(""))
+    toc()
 
     # check for missing street numbers in building column
+    tic("check buildings")
     df <- df |> check_building(street_number, street_number_multi, building, addressr_id)
+    toc()
 
     # tidy up for output
+    tic("tidy output")
     df <- df |>
       mutate({{ input_column }} := na_if({{ input_column }}, "")) |>
       rename("street_name" = {{ input_column }}) |>
       arrange({{ original_row_id }}, {{ addressr_id }}) |>
       unite("clean_address", c("street_number", "street_number_multi", "street_number_fraction", "pre_direction", "street_name", "street_suffix", "post_direction"), sep = " ", remove = FALSE, na.rm = TRUE)
+    toc()
+
+    df
 
   } else if (dataset == "default_db") {
 
